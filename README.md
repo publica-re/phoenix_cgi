a50a836d07db93391df7cfaba0fc428c7fbeee60
David Baumgartner <dv@bmgr.me>
2020-08-30 16:05:30 +0200

    🔖 preparing for production

--- README.md
+++ README.md
@@ -67,3 +67,28 @@ scope "/" do
     binary: "/usr/lib/git-core/git-http-backend"
 end
 ```
+
+## Documentation
+
+The documentation is available on [hexdocs](https://hexdocs.pm/phoenix_cgi/0.1.0/PhoenixCGI.html#content).
+
+## Licence
+
+Copyright (C) 2020 David Baumgartner
+
+This program is free software: you can redistribute it and/or modify it
+under the terms of the GNU General Public License as published by the Free
+Software Foundation, either version 3 of the License, or (at your option)
+any later version.
+
+This program is distributed in the hope that it will be useful, but WITHOUT
+ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
+more details.
+
+You should have received a copy of the GNU General Public License along
+with this program. If not, see <http://www.gnu.org/licenses/>.
+
+## Contribution
+
+I happily accept pull requests through [GitHub](https://github.com/publica-re/phoenix_cgi).
--- mix.exs
+++ mix.exs
@@ -4,13 +4,16 @@ defmodule PhoenixCgi.MixProject do
   def project do
     [
       app: :phoenix_cgi,
+      name: "PhoenixCGI",
       version: "0.1.0",
       elixir: "~> 1.7",
       elixirc_paths: elixirc_paths(Mix.env()),
       compilers: Mix.compilers(),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
-      deps: deps()
+      deps: deps(),
+      source_url: "https://github.com/publica-re/phoenix_cgi",
+      package: package()
     ]
   end
 
@@ -25,6 +28,7 @@ defmodule PhoenixCgi.MixProject do
 
   defp deps do
     [
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:porcelain, "~> 2.0"},
       {:plug, "~> 1.10"}
     ]
@@ -32,5 +36,14 @@ defmodule PhoenixCgi.MixProject do
 
   defp aliases do
     []
+  end
+
+  defp package() do
+    [
+      name: "phoenix_cgi",
+      description: "A small CGI controller for Phoenix",
+      licenses: ["GPL-3.0-or-later"],
+      links: %{"GitHub" => "https://github.com/publica-re/phoenix_cgi", "Author" => "https://publica.re"}
+    ]
   end
 end
